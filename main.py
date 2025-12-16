f2532a8f4b9e2b7eb39ba63522dda5333f6e47ea
Mohammad Moslem Uddin <pal.uddin@gmail.com>
2025-12-15 21:55:07 -0500

    limit the timeframe to 60 FPS

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_HEIGHT))
-
+    clock_obj = pygame.time.Clock()
+    dt = 0
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while (True):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+                 
         screen.fill("black")
-    
-    display.flip()
-    
-    # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock_obj.tick(60)/ 1000
+  
+
 
 
 if __name__ == "__main__":
